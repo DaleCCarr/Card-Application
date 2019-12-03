387b8aca04a4e8f1ab3e5f33eaf2e8c83a32dbb8
Dale <dale.carr1@outlook.com>
2019-12-03 10:19:53 +0000

    Created the basic HTML outline of the card.

--- src/_card/component/Card.js
+++ src/_card/component/Card.js
@@ -6,9 +6,14 @@ function Card() {
   return (
     <div className="main-card">
       <div className="inner-outline">
-        <p className="name">Dale Carr</p>
+        <p className=" basic-text name">Dale Carr</p>
         <img className= "profile-pic" src={defaultPic} alt = "profile picture for the user"/>
         </div>
+        <p className= "description-text basic-text">Tackle matey ballast rum nipperkin doubloon cutlass fore hempen halter lanyard. Mutiny gally rutters run a shot across the bow Sail ho cutlass reef sails driver salmagundi sutler. Rope's end case shot poop deck topgallant Gold Road grog reef pinnace rum jury mast.
+        Tackle matey ballast rum nipperkin doubloon cutlass fore hempen halter lanyard. Mutiny gally rutters run a shot across the bow Sail ho cutlass reef sails driver salmagundi sutler. Rope's end case shot poop deck topgallant Gold Road grog reef pinnace rum jury mast.
+        Tackle matey ballast rum nipperkin doubloon cutlass fore hempen halter lanyard. Mutiny gally rutters run a shot across the bow Sail ho cutlass reef sails driver salmagundi sutler. Rope's end case shot poop deck topgallant Gold Road grog reef pinnace rum jury mast.
+        </p>
+
     </div>
   );
 }
